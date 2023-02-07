ac1b64f5d955d257248a491f5419b0d00384465e
Robert162001 <badalutarobert@gmail.com>
2023-02-07 22:33:16 +0200

    refactor the property file

--- src/test/java/utils/PropertiesReader.java
+++ src/test/java/utils/PropertiesReader.java
@@ -1,19 +1,20 @@
 package utils;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Locale;
 import java.util.Properties;
 
 public class PropertiesReader {
 
-    private static final String TEST_PROPERTIES = "test.properties";
+    private static final String TEST_PROPERTIES = "src/test/resources/test.properties";
 
-    public enum TestProperty {BASE_URL, USERNAME}
+    public enum TestProperty {BASE_URL}
 
     public static String getProperty(TestProperty property) {
         Properties properties = new Properties();
         try {
-            properties.load(ClassLoader.getSystemResource(TEST_PROPERTIES).openStream());
+            properties.load(new FileInputStream(TEST_PROPERTIES));
         } catch (IOException fie) {
             fie.printStackTrace();
         }
