6d222462284176f4b87eeb76ca146730a053a603
Robert162001 <badalutarobert@gmail.com>
2023-02-09 16:09:43 +0200

    integrate log4j2

--- src/test/java/step_definitions/StepCucumber.java
+++ src/test/java/step_definitions/StepCucumber.java
@@ -5,11 +5,15 @@ import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.Assert;
 import testutils.BaseTestProvider;
 import webpages.Pages;
 
+
 public class StepCucumber {
+    private static final Logger logger = LogManager.getLogger(StepCucumber.class);
 
     private BaseTestProvider baseTestProvider;
     private Pages pages;
@@ -34,22 +38,21 @@ public class StepCucumber {
 
     @When("They navigate to Contact page")
     public void navigateToContactPage() {
+        logger.info("Click on Contact");
         pages.homePage().clickOnContact();
     }
 
     @Then("They should be able to chat with customer support")
-    public void verifyChatIsEnabled() {
-        Assert.assertTrue(pages.contactPage().isOnContactPage());
-    }
+    public void verifyChatIsEnabled(){Assert.assertTrue(pages.contactPage().isOnContactPage());}
 
 
     @When("They navigate to About page")
     public void navigateToAboutPage() {
+        logger.info("Click on About");
         pages.homePage().clickOnAbout();
     }
 
     @Then("They should see information about site")
-    public void verifySiteInformation() {
-        Assert.assertTrue(pages.aboutPage().isOnAboutPage());
+    public void verifySiteInformation() {Assert.assertTrue(pages.aboutPage().isOnAboutPage());
     }
 }
